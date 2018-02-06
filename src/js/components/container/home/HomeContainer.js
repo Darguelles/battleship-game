38a85e73a20e00c7e00b8ee655c724cbe4c2c417
darguelles.rojas91@gmail.com <darguelles.rojas91@gmail.com>
2018-02-06 18:03:38 -0200

    Application use Materialize CSS as main stylesheet library

--- src/js/components/container/home/HomeContainer.js
+++ src/js/components/container/home/HomeContainer.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import {Button, Icon} from 'react-materialize'
 
 class HomeContainer extends Component {
     constructor() {
@@ -12,10 +13,16 @@ class HomeContainer extends Component {
     render() {
         return (
             <div id="home-container">
-                Home container ready!
+
+                <Button waves='light'>New game<Icon left>save</Icon></Button>
+                <Button waves='light'>Load game<Icon left>save</Icon></Button>
+
             </div>
         );
     }
 }
 
 export default HomeContainer;
+
+const wrapper = document.getElementById("main-content");
+wrapper ? ReactDOM.render(<HomeContainer/>, wrapper) : false;
